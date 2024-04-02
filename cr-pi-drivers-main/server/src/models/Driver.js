ffc5c2b1ff3d8e3d5cb3ada543646ebcdf49df06
MGoretta <maugoretta@gmail.com>
2024-04-02 14:44:20 -0300

    push día 2

--- cr-pi-drivers-main/server/src/models/Driver.js
+++ cr-pi-drivers-main/server/src/models/Driver.js
@@ -4,8 +4,34 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Driver', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    nacionality: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dob: {
+      type: DataTypes.DATEONLY,
       allowNull: false,
     },
   });
