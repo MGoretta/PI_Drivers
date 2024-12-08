1793655bcb34075c3d659509f133f9e42be34373
MGoretta <maugoretta@gmail.com>
2024-12-08 11:34:16 -0300

    cambios en landing

--- cr-pi-drivers-main/client/src/Views/Landing/Landing.jsx
+++ cr-pi-drivers-main/client/src/Views/Landing/Landing.jsx
@@ -1,9 +1,10 @@
 import "./Landing.css";
 import { Link } from "react-router-dom";
+import fondoLanding from "../../assets/fondolanding.jpg";
 
 const Landing = () => {
   return (
-    <div className="landing">
+    <div className="landing" style={{ backgroundImage: `url(${fondoLanding})` }}>
       <div className='containerLanding'>
       <Link to="/home">
         <button className="buttonLanding">Run here!</button>
